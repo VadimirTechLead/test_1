bc43a5643a22df32bea554ed823975f41f1b25d1
Юлия Антоненко <slonik6663444@gmail.com>
2018-01-31 22:06:34 +0200

    add public

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const my_twitter = require("my_twitter");
-var app = express();
+const serveStatic = require("serve-static");
 
-app.get("/", async (req, res) => {
-  var params = { q: "nodejs" };
-  let response = await my_twitter.get("search/tweets", params);
-  res.send(response);
+const app = express();
+
+app.use(serveStatic("public", { index: "index.html" }));
+
+app.get("/:q", (req, res) => {
+  my_twitter
+    .get("search/tweets", req.params)
+    .then(r => {
+      res.send(r.statuses);
+    })
+    .catch(err => {
+      res.status(400).send("Sorry cant find that! 400");
+    });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry cant find that!");
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(3000);
